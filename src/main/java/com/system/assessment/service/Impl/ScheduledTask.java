74b0a08fc0646a2998c3d73e1e7d51bd3b05d5b6
piaozhenyu0905 <2339134840@qq.com>
2025-01-03 12:28:07 +0800

    2025-1-3 提交

--- src/main/java/com/system/assessment/service/Impl/ScheduledTask.java
+++ src/main/java/com/system/assessment/service/Impl/ScheduledTask.java
@@ -1,10 +1,13 @@
 package com.system.assessment.service.Impl;
 
 import com.system.assessment.constants.ProcessType;
+import com.system.assessment.exception.CustomExceptionType;
+import com.system.assessment.exception.ResponseResult;
 import com.system.assessment.mapper.EvaluateMapper;
 import com.system.assessment.mapper.TodoListMapper;
 import com.system.assessment.pojo.EvaluateProcess;
 import com.system.assessment.service.EmailService;
+import com.system.assessment.service.EvaluateService;
 import com.system.assessment.service.ProcessService;
 import com.system.assessment.vo.EmailVO;
 import com.system.assessment.vo.NotCompletedSet;
@@ -32,8 +35,21 @@ public class ScheduledTask {
     @Autowired
     public EmailService emailService;
 
+    @Autowired
+    public EvaluateService evaluateService;
+
     @Scheduled(cron = "0 0 8,13 * * ?")
     public void noticeAllNotCompleted(){
+        //先判断是否处于打分环节
+        Integer newEnableProcess = evaluateService.findNewEnableProcess();
+        if(newEnableProcess == null){
+            newEnableProcess = 0;
+        }
+        //不处于打分环节则不进行提醒
+        if(!newEnableProcess.equals(ProcessType.Evaluation.getCode())){
+           return;
+        }
+
         //1.找出所有未打完分的人的邮箱
         Integer newEpoch = evaluateMapper.findNewEpoch();
         List<NotCompletedSet> allNotCompletedId = todoListMapper.findAllNotCompleted(newEpoch);
